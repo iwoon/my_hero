c768b9b0caf185b3f1e8beaf6f2a03281439dad0
iBlurBlur <tanakorn.ngam@gmail.com>
2021-03-04 09:58:22 +0700

    binding data table from node.js

--- frontend/src/app/stock/stock-home/stock-home.component.ts
+++ frontend/src/app/stock/stock-home/stock-home.component.ts
@@ -2,49 +2,24 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ProductResponse } from 'src/app/models/product.model';
 import { NetworkService } from 'src/app/services/network.service';
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = [
-  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-  'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
-
-/**
- * @title Data table with sorting, pagination, and filtering.
- */
 @Component({
   selector: 'app-stock-home',
   templateUrl: './stock-home.component.html',
   styleUrls: ['./stock-home.component.css']
 })
 export class StockHomeComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['productId', 'name', 'price', 'stock', 'action'];
+  dataSource = new MatTableDataSource<ProductResponse>();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private networkService: NetworkService
-  ) {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-  }
+  ) {}
 
   ngOnInit(): void {
     this.feedProduct();
@@ -58,14 +33,13 @@ export class StockHomeComponent implements OnInit, AfterViewInit {
   feedProduct(): void {
     this.networkService.getProducts().subscribe(
       result => {
-        alert(result.length)
+        this.dataSource.data = result;
       },
       error => {
         alert(JSON.stringify(error))
       }
     );
   }
-
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -75,17 +49,4 @@ export class StockHomeComponent implements OnInit, AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
 }
